4e7cf337c507432bf00f4ef6707a5f097b2e6a1a
choyyima <choyyima.aja@gmail.com>
2016-11-20 22:34:13 +0700

    function upload excel success

---
+++ act/subtesteeAdd.php
@@ -0,0 +1,84 @@
+<?php
+
+include '../config.php';
+include '../class/excel_reader2.php';
+
+// membaca file excel yang diupload
+$data = new Spreadsheet_Excel_Reader($_FILES['subtestee']['tmp_name']);
+
+// membaca jumlah baris dari data excel
+$baris = $data->rowcount($sheet_index = 0);
+
+// nilai awal counter untuk jumlah data yang sukses dan yang gagal diimport
+$sukses = 0;
+$gagal = 0;
+
+// import data excel mulai baris ke-2 (karena baris pertama adalah nama kolom)
+for ($i = 3; $i <= $baris; $i++) {
+    $idSkala = $_POST['skala'];
+    $nama = $data->val($i, 2);
+    $iq = $data->val($i, 3);
+    $se = $data->val($i, 4);
+    $wa = $data->val($i, 5);
+    $an = $data->val($i, 6);
+    $ge = $data->val($i, 7);
+    $ra = $data->val($i, 8);
+    $zr = $data->val($i, 9);
+    $fa = $data->val($i, 10);
+    $wu = $data->val($i, 11);
+    $me = $data->val($i, 12);
+    $G = $data->val($i, 13);
+    $L = $data->val($i, 14);
+    $I = $data->val($i, 15);
+    $T = $data->val($i, 16);
+    $V = $data->val($i, 17);
+    $S = $data->val($i, 18);
+    $R = $data->val($i, 19);
+    $D = $data->val($i, 20);
+    $C = $data->val($i, 21);
+    $E = $data->val($i, 22);
+    $N = $data->val($i, 23);
+    $A = $data->val($i, 24);
+    $P = $data->val($i, 25);
+    $X = $data->val($i, 26);
+    $B = $data->val($i, 27);
+    $O = $data->val($i, 28);
+    $Z = $data->val($i, 29);
+    $K = $data->val($i, 30);
+    $F = $data->val($i, 31);
+    $W = $data->val($i, 32);
+    $mot = $data->val($i, 33);
+    $ini = $data->val($i, 34);
+    $PD = $data->val($i, 35);
+    $emo = $data->val($i, 36);
+    $ses = $data->val($i, 37);
+    $sos = $data->val($i, 38);
+    $pat = $data->val($i, 39);
+    $tel = $data->val($i, 40);
+    $han = $data->val($i, 41);
+    $jeg = $data->val($i, 42);
+    $msdt = $data->val($i, 43);
+
+    // setelah data dibaca, sisipkan ke dalam tabel mhs
+    $query = "INSERT INTO subtestee_testee "
+            . "(id_skala, nama, iq, se, wa, an, ge, ra, zr, fa, wu, me, g, l, i,t,"
+            . " v, s, r, d, c, e, n, a, p, x, b, o, z, k, f, w, mot, ini, pd, emo, ses, sos,"
+            . " pat, tel, han, jeg, msdt) "
+            . "VALUES ('$idSkala', '$nama', '$iq', '$se', '$wa', '$an', '$ge', '$ra', '$zr', '$fa',"
+            . " '$wu', '$me', '$G', '$L', '$I', '$t', '$V', '$S', '$R', '$D', '$C', '$E', '$N', '$A', '$P', '$X', '$B',"
+            . " '$O', '$Z', '$K', '$F', '$W', '$mot', '$ini','$PD', '$emo', '$ses', '$sos', '$pat', '$tel', '$han', '$jeg', '$msdt')";
+    $hasil = mysql_query($query);
+
+    // jika proses insert data sukses, maka counter $sukses bertambah
+    // jika gagal, maka counter $gagal yang bertambah
+    if ($hasil) {
+        $sukses++;
+    } else {
+        $gagal++;
+    }
+}
+
+// tampilan status sukses dan gagal
+echo "<h3>Proses import data selesai.</h3>";
+echo "<p>Jumlah data yang sukses diimport : " . $sukses . "<br>";
+echo "Jumlah data yang gagal diimport : " . $gagal . "</p>";
--- subtestee.php
+++ subtestee.php
@@ -1,17 +1,131 @@
 <div class="page-header">
     <h1>
         Subtest Testee
-        <small>
-            <i class="ace-icon fa fa-angle-double-right"></i>
-            overview &amp; stats
-        </small>
     </h1>
 </div><!-- /.page-header -->
 
 <div class="row">
     <div class="col-xs-12">
-        <div class="row">
+        <!-- PAGE CONTENT BEGINS -->
+
+        <p><a class="btn btn-success" data-target="#ModalAdd" data-toggle="modal">Add Data</a></p>      
+        <table id="example" class="table table-striped table-bordered">
+            <thead>
+            <th>No.</th>
+            <th>Kategori</th>
+            <th>Action</th>
+            </thead>
+            <?php
+            //menampilkan data mysqli
+            $no = 0;
+            $qSkala = mysql_query("SELECT * FROM kategori");
+            while ($dataSkala = mysql_fetch_array($qSkala)) {
+                $no++;
+                ?>
+                <tr>
+                    <td><?php echo $no; ?></td>
+                    <td><?php echo $dataSkala['nama']; ?></td>
+                    <td>
+                        <a href="#" class='btn btn-xs btn-info open_modal' id='<?php echo $dataSkala['id']; ?>'><i class="ace-icon fa fa-pencil bigger-120"></i></a> 
+                        <a href="#" class='btn btn-xs btn-danger'onclick="confirm_modal('subtesteeDel.php?&skala_id=<?php echo $dataSkala['id']; ?>');"><i class="ace-icon fa fa-trash bigger-120"></i></a>
+                    </td>
+                </tr>
+            <?php } ?>
+        </table>
+
+        <!-- Modal Popup untuk Add--> 
+        <div id="ModalAdd" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                        <h4 class="modal-title" id="myModalLabel">Testee</h4>
+                    </div>
+
+                    <div class="modal-body">
+                        <form action="act/subtesteeAdd.php" name="modal_popup" enctype="multipart/form-data" method="POST">
+
+                            <div class="form-group" style="padding-bottom: 20px;">
+                                <label for="Modal Name">Skala</label>
+                                <?php $qSkala = mysql_query("SELECT * FROM skala"); ?>
+                                <select class="form-control" name="skala" id="form-field-select-1">
+                                    <?php while ($dSkla = mysql_fetch_array($qSkala)) { ?>
+                                        <option value="<?php echo $dSkla['id'] ?>"><?php echo $dSkla['nama'] ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+
+                            <div class="form-group" style="padding-bottom: 20px;">
+                                <label>Upload</label>
+                                <!--<div class="col-xs-12">-->
+                                    <input type="file" name="subtestee" class="form-control" />
+                                <!--</div>-->
+                            </div>
+
+                            <div class="modal-footer">
+                                <button class="btn btn-success" type="submit">
+                                    Confirm
+                                </button>
+
+                                <button type="reset" class="btn btn-danger"  data-dismiss="modal" aria-hidden="true">
+                                    Cancel
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal Popup untuk Edit--> 
+        <div id="ModalEdit" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 
         </div>
+
+        <!-- Modal Popup untuk delete--> 
+        <div class="modal fade" id="modal_delete">
+            <div class="modal-dialog">
+                <div class="modal-content" style="margin-top:100px;">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title" style="text-align:center;">Are you sure to delete this information ?</h4>
+                    </div>
+
+                    <div class="modal-footer" style="margin:0px; border-top:0px; text-align:center;">
+                        <a href="#" class="btn btn-danger" id="delete_link">Delete</a>
+                        <button type="button" class="btn btn-success" data-dismiss="modal">Cancel</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- PAGE CONTENT ENDS -->
     </div><!-- /.col -->
 </div><!-- /.row -->
+
+<script src="assets/js/jquery.2.1.1.min.js"></script>
+<!-- Javascript untuk popup modal Edit--> 
+<script type="text/javascript">
+                            $(document).ready(function () {
+                                $(".open_modal").click(function (e) {
+                                    var m = $(this).attr("id");
+                                    $.ajax({
+                                        url: "modKategoriEdit.php",
+                                        type: "GET",
+                                        data: {modal_id: m},
+                                        success: function (ajaxData) {
+                                            $("#ModalEdit").html(ajaxData);
+                                            $("#ModalEdit").modal('show', {backdrop: 'true'});
+                                        }
+                                    });
+                                });
+                            });
+//<!-- Javascript untuk popup modal Delete--> 
+                            function confirm_modal(delete_url)
+                            {
+                                $('#modal_delete').modal('show', {backdrop: 'static'});
+                                document.getElementById('delete_link').setAttribute('href', delete_url);
+                            }
+</script>
